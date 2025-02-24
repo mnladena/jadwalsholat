d092bba4c17c060d9121c5d88e0f2bfa5dbdd78e
mnladena <mnl.adena@gmail.com>
2025-02-24 19:45:14 +0700

    update layout dan js fetch data

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: { plugins: [tailwindcss()] },
+  adapter: vercel(),
 });
